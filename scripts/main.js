93022be5d593cd6778c3d99245f120af114dc809
Tri Phan <tphan11@hawk.iit.edu>
2025-05-27 15:25:54 -0500

    added third screen and navigation

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,10 @@
 const startBtn = document.getElementById("starting-button");
 const startScreen = document.getElementById("start-screen");
+const flashcardScreen = document.getElementById("flashcard-screen")
 const menueScreen = document.getElementById("menue-screen");
 const backBtn = document.getElementById("Right-Arrow-Icon");
+const newSet = document.getElementById("new-set-icon");
+const backBtnFlash = document.getElementById("go-back-flashcard");
 
 startBtn.addEventListener('click', () => { 
     startScreen.style.display = 'none';
@@ -12,4 +15,48 @@ backBtn.addEventListener('click', () => {
     menueScreen.style.display = 'none';
     startScreen.style.display = 'flex';
 });
+
+newSet.addEventListener("click", () => {
+    menueScreen.style.display = "none";
+    flashcardScreen.style.display = "flex";
+})
+
+backBtnFlash.addEventListener("click", () => {
+    flashcardScreen.style.display = "none";
+    menueScreen.style.display = "block";
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
