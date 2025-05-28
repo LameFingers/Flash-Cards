95d3b0491baeb232ae72397e873bca8b78ddfc37
Tri Phan <tripkhan005@gmail.com>
2025-05-28 10:11:31 -0500

    now can actually add cards

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,46 @@
-const startBtn = document.getElementById("starting-button");
-const startScreen = document.getElementById("start-screen");
-const flashcardScreen = document.getElementById("flashcard-screen")
-const menueScreen = document.getElementById("menue-screen");
-const backBtn = document.getElementById("Right-Arrow-Icon");
-const newSet = document.getElementById("new-set-icon");
-const backBtnFlash = document.getElementById("go-back-flashcard");
+document.addEventListener("DOMContentLoaded", () => {
+    const startBtn = document.getElementById("starting-button");
+    const startScreen = document.getElementById("start-screen");
+    const flashcardScreen = document.getElementById("flashcard-screen")
+    const menueScreen = document.getElementById("menue-screen");
+    const backBtn = document.getElementById("Right-Arrow-Icon");
+    const newSet = document.getElementById("new-set-icon");
+    const backBtnFlash = document.getElementById("go-back-flashcard");
+    const addCardBtn = document.getElementById("add-card");
+    const flashcardContent = document.getElementById("flashcard-content");
 
-startBtn.addEventListener('click', () => { 
-    startScreen.style.display = 'none';
-    menueScreen.style.display = 'block';
+    startBtn.addEventListener('click', () => { 
+        startScreen.style.display = 'none';
+        menueScreen.style.display = 'block';
+    });
+
+    backBtn.addEventListener('click', () => {
+        menueScreen.style.display = 'none';
+        startScreen.style.display = 'flex';
+    });
+
+    newSet.addEventListener("click", () => {
+        menueScreen.style.display = "none";
+        flashcardScreen.style.display = "flex";
+    })
+
+    backBtnFlash.addEventListener("click", () => {
+        flashcardScreen.style.display = "none";
+        menueScreen.style.display = "block";
+    })
+
+    addCardBtn.addEventListener("click", () => {
+        const newCard = document.createElement('div');
+        newCard.className = "card-container";
+        newCard.innerHTML = `
+            <input type="text" class="term" placeholder="Term">
+            <input type="text" class="definition" placeholder="Definition">
+        `;
+        const buttonContainer = document.querySelector(".button-container");
+        flashcardContent.insertBefore(newCard, buttonContainer);
+    });
+
 });
-
-backBtn.addEventListener('click', () => {
-    menueScreen.style.display = 'none';
-    startScreen.style.display = 'flex';
-});
-
-newSet.addEventListener("click", () => {
-    menueScreen.style.display = "none";
-    flashcardScreen.style.display = "flex";
-})
-
-backBtnFlash.addEventListener("click", () => {
-    flashcardScreen.style.display = "none";
-    menueScreen.style.display = "block";
-})
-
-
-
 
 
 
