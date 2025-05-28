2772c21ce02291c902f016798f6dbad73512ad01
Tri Phan <tripkhan005@gmail.com>
2025-05-28 10:23:41 -0500

    more updates

--- scripts/main.js
+++ scripts/main.js
@@ -30,12 +30,26 @@ document.addEventListener("DOMContentLoaded", () => {
     })
 
     addCardBtn.addEventListener("click", () => {
-        const newCard = document.createElement('div');
-        newCard.className = "card-container";
+        const cardContainers = document.querySelectorAll(".card-container");
+        const lastCard = cardContainers[cardContainers.length - 1];
+        const termInput = lastCard.querySelector(".term");
+        const definitionInput = lastCard.querySelector(".definition");
+        const term = termInput.value.trim();
+        const definition = definitionInput.value.trim();
+
+        if (term === " " || definition === "") {
+            alert("Please fill in both the term and definition.");
+            return;
+        }
+
+        const newCard = document.createElement("div");
+        newCard.classList.add("card-container");
+
         newCard.innerHTML = `
             <input type="text" class="term" placeholder="Term">
             <input type="text" class="definition" placeholder="Definition">
         `;
+
         const buttonContainer = document.querySelector(".button-container");
         flashcardContent.insertBefore(newCard, buttonContainer);
     });
